3a49a62abcb78844b156e59c747ce8dbd2871876
David Wilson <david@dvdwlsn.com>
2017-02-01 20:00:04 -0500

    Fix scrolling to expanded press
    +lint

--- assets/js/src/main/press.js
+++ assets/js/src/main/press.js
@@ -2,10 +2,11 @@ $(document).ready(function () {
   var $html = $('html, body');
   var $window = $(window);
   var $featured = $('.featured-press');
-  var $unfeatured = $('.press-group .unfeatured');
+  var $unfeatured = $('.press-section .unfeatured');
 
-  function scrollTo () {
-    $html.animate({scrollTop: $window.scrollTop() + $featured.height() + 100 }, '400');
+  function scrollTo() {
+    $html.animate({ scrollTop: $window.scrollTop() + $featured.height() + 100, }, '400');
+    console.log('wtf');
   }
 
   $unfeatured.on('shown.bs.collapse', $.debounce(100, scrollTo));
