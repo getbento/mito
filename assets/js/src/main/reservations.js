54b42a8ad841c884c7fa66439224a98af944205c
Pierre Drescher <pierre.drescher@gmail.com>
2018-06-25 12:10:27 -0400

    Update reservations to link to new page for seatme

--- assets/js/src/main/reservations.js
+++ assets/js/src/main/reservations.js
@@ -1,45 +1,5 @@
 $(document).ready(function() {
   window.Reservations = {
-    initializeSeatmeModal: function(seatmeId) {
-      Reservations.seatmeId = seatmeId;
-      var reservationsButton = $('[data-target="#modal-seatme"]');
-      reservationsButton.on('click', Reservations.showSeatmeModal);
-      reservationsButton.removeAttr('data-toggle');
-    },
-
-    showSeatmeModal: function(event) {
-      event.preventDefault();
-
-      var url = 'https://www.yelp.com/reservations/' + Reservations.seatmeId;
-      var isMobile = window.matchMedia('only screen and (max-width: 760px)');
-
-      if (isMobile.matches) {
-        window.open(url, '_blank');
-        return;
-      }
-
-      var reservationsModal = $('#modal-seatme');
-
-      reservationsModal.modal('show');
-
-      var modalBody = reservationsModal.find('div.modal-content');
-
-      var iframeExists = $('#seatmeIframe').length > 0;
-
-      if (iframeExists) {
-        return;
-      }
-
-      var seatmeIframe = $('<iframe>', {
-        src: url,
-        id: 'seatmeIframe',
-        width: '100%',
-        height: '570px',
-        frameborder: 0
-      });
-      modalBody.append(seatmeIframe);
-    },
-
     initializeOpenTableFireFoxFix: function(opentableId) {
       // check if browser is Firefox - return if not
       var isFirefox = typeof InstallTrigger !== 'undefined';
